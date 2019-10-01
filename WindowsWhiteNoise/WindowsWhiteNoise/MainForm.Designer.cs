c40f208ac031241e7ebe56425e729de6ce7263af
Stefan Alenius <darlanio@gmail.com>
2019-10-02 00:21:00 +0200

    Just a hack that makes white noise video
    This hack makes white noise video, but need about 5 minutes on a reasonable hardware to start up. 
    No multithreading, which should speed things up, but that is for a day in the future.
    It uses a buffer to create 180 random images with white and black pixels, which it copy to the screen 30 times per second when running.

---
+++ WindowsWhiteNoise/WindowsWhiteNoise/MainForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsWhiteNoise
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainPanel.Location = new System.Drawing.Point(0, -1);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(802, 452);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.MainPanel_Paint);
+            this.mainPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseClick);
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Interval = 30;
+            this.mainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "MainForm";
+            this.Text = "WindowsWhiteNoise";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Timer mainTimer;
+    }
+}
+
---
+++ WindowsWhiteNoise/WindowsWhiteNoise/MainForm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsWhiteNoise
+{
+    public partial class MainForm : Form
+    {
+        private Random _random = new Random();
+
+        // GDI32 used for fallback and is slower than prepared bitmaps.
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        internal static extern bool SetPixel(IntPtr hdc, int X, int Y, uint crColor);
+
+        private Boolean ticking = false;
+        private Bitmap[] bitmaps;
+        int bitmapCounter = 0;
+        int [] lastBitmapCounters;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SetDoubleBuffered(mainPanel);
+
+            bitmaps = null;
+            bitmaps = CreateRandomBitmaps(bitmaps, 180, 1920, 1080);
+            lastBitmapCounters = new int[bitmaps.Length];
+            for (int i = 0; i < lastBitmapCounters.Length; i++) lastBitmapCounters[i] = i;
+            mainTimer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Picks a new random bitmap and remembers the ones that has been used, avoiding to pick the most recently used.
+        /// </summary>
+        private void UpdateBitmapCounter()
+        {
+            int n,r;
+
+            r = _random.Next() % (bitmaps.Length / 2);
+            n = lastBitmapCounters[r];
+            bitmapCounter = n;
+            for(int i=r;i<lastBitmapCounters.Length-1;i++)
+            {
+                lastBitmapCounters[i] = lastBitmapCounters[i + 1];
+            }
+            lastBitmapCounters[lastBitmapCounters.Length - 1] = n;
+        }
+
+        /// <summary>
+        /// Creates specified number of bitmaps with specified dimensions for later use
+        /// </summary>
+        /// <param name="bms"></param>
+        public Bitmap[] CreateRandomBitmaps(Bitmap[] bms, int nob, int maxx, int maxy)
+        {
+            bms = new Bitmap[nob];
+            for(int i=0;i<nob;i++)
+            {
+                bms[i] = new Bitmap(maxx, maxy);
+                using (Graphics gr = Graphics.FromImage(bms[i]))
+                {
+                    for (int y = 0; y < maxy; y++)
+                    {
+                        for (int x = 0; x < maxx;)
+                        {
+                            uint randomBits = (uint)_random.Next();
+                            for (int x2 = x + 24; x < x2; x++)
+                            {
+                                Brush useThisBrush = System.Drawing.Brushes.White;
+                                if ((randomBits & (uint)1) == 1)
+                                {
+                                    useThisBrush = System.Drawing.Brushes.Black;
+                                }
+                                randomBits = randomBits >> 1;
+                                gr.FillRectangle(useThisBrush, x, y, 1, 1);
+                            }
+                        }
+                    }
+
+                }
+            }
+            return bms;
+        }
+
+
+
+        // FROM: http://stackoverflow.com/questions/76993/how-to-double-buffer-net-controls-on-a-form
+        /// <summary>
+        /// SetDoubleBuffered
+        /// Makes the window update without flickering - .NET doublebuffer
+        /// </summary>
+        /// <param name="c"></param>
+        public static void SetDoubleBuffered(System.Windows.Forms.Control c)
+        {
+            //Taxes: Remote Desktop Connection and painting
+            //http://blogs.msdn.com/oldnewthing/archive/2006/01/03/508694.aspx
+            if (System.Windows.Forms.SystemInformation.TerminalServerSession)
+                return;
+
+            System.Reflection.PropertyInfo aProp =
+                  typeof(System.Windows.Forms.Control).GetProperty(
+                        "DoubleBuffered",
+                        System.Reflection.BindingFlags.NonPublic |
+                        System.Reflection.BindingFlags.Instance);
+
+            aProp.SetValue(c, true, null);
+        }
+
+
+
+        private void MainPanel_Paint(object sender, PaintEventArgs e)
+        {
+            int renderWidth = (int) e.Graphics.ClipBounds.Width;
+            int renderHeight = e.ClipRectangle.Height;
+
+            if (bitmaps == null)
+            {
+                // Fallback to slow routine if no bitmaps have been created
+                uint white = (uint)((255 << 16) | (255 << 8) | (255));
+                uint black = (uint)0;
+                IntPtr hdc = e.Graphics.GetHdc();
+                for (int y = 0; y < renderHeight; y++)
+                {
+                    for (int x = 0; x < renderWidth;)
+                    {
+                        // Color pixelColor = GetPixelColor(x, y);
+                        // NOTE: GDI colors are BGR, not ARGB.                    
+                        //uint colorRef = (uint)((pixelColor.B << 16) | (pixelColor.G << 8) | (pixelColor.R));
+
+                        uint randomBits = (uint)_random.Next();
+                        for (int x2 = x + 24; x < x2; x++)
+                        {
+                            uint colorRef = white;
+                            if ((randomBits & (uint)1) == 1)
+                            {
+                                colorRef = black;
+                            }
+                            randomBits = randomBits >> 1;
+                            SetPixel(hdc, x, y, colorRef);
+                        }
+                    }
+                }
+                e.Graphics.ReleaseHdc(hdc);
+            } else
+            {
+                // Fast routine, using premade bitmaps
+                e.Graphics.DrawImage(bitmaps[bitmapCounter], 0, 0);
+                UpdateBitmapCounter();
+            }
+            ticking = false; // Paint is done, reenabling the timer
+        }
+
+        /// <summary>
+        /// Mouse Click Handler - toggles fullscreen mode / window mode
+        /// </summary>
+        /// <param name="sender">the object sending the mouseclick</param>
+        /// <param name="e">MouseEventArgs</param>
+        private void MainPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (this.FormBorderStyle == FormBorderStyle.None)
+            {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+                if (this.WindowState == FormWindowState.Maximized) this.WindowState = FormWindowState.Normal;
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            if(!ticking)
+            {
+                ticking = true; // When Paint is done, this is set to false
+                mainPanel.Refresh();
+            }
+        }
+    }
+}
